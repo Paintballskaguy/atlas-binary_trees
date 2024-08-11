6f22305d5104d30f4156ceb08eef5bc8c5ce15d3
Paintballskaguy <Johnstournament@gmail.com>
2024-08-11 16:33:27 -0500

    declared the int at the begining.

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -9,12 +9,15 @@
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
+	int left_height;
+	int right_height;
+
 	if (tree == NULL)
 		return (0);
 
 	/* Calculate the height of the left and right subtrees */
-	int left_height = tree->left ? (int)binary_tree_height(tree->left) + 1 : 0;
-	int right_height = tree->right ? (int)binary_tree_height(tree->right) + 1 : 0;
+	left_height = tree->left ? (int)binary_tree_height(tree->left) + 1 : 0;
+	right_height = tree->right ? (int)binary_tree_height(tree->right) + 1 : 0;
 
 	/* Return the balance factor */
 	return (left_height - right_height);
