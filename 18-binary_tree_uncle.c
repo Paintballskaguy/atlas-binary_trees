63f6f04603b10229bce8eaa2a154d4c83bc65208
Paintballskaguy <Johnstournament@gmail.com>
2024-08-11 16:54:02 -0500

    betty!! why!!!??

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -29,8 +29,10 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
  * binary_tree_uncle - Finds the uncle of a node
  * @node: Pointer to the node to find the uncle
  *
- * Return: Pointer to the uncle node, or NULL if no uncle exists or node is NULL
+ * Return: Pointer to the uncle node,
+ * or NULL if no uncle exists or node is NULL
  */
+
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
 	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
